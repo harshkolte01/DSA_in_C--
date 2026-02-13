f20a884886bab1e3e4b9ee2073ba1578bd81c2c6
Harsh Kolte <74823522+harshkolte01@users.noreply.github.com>
2026-02-13 22:51:10 +0530

    book allocation binary problem

--- Lec_14(binarySearch(/Code4.cpp
+++ Lec_14(binarySearch(/Code4.cpp
@@ -15,7 +15,7 @@ Return the single element that appears only once.
 
 Your solution must run in O(log n) time and O(1) space.
 
- 
+
 
 Example 1:
 
@@ -25,7 +25,7 @@ Example 2:
 
 Input: nums = [3,3,7,7,10,11,11]
 Output: 10
- 
+
 
 Constraints:
 
@@ -33,15 +33,18 @@ Constraints:
 0 <= nums[i] <= 105
 */
 
-class Solution {
+class Solution
+{
 public:
-    int singleNonDuplicate(vector<int>& nums) {
+    int singleNonDuplicate(vector<int> &nums)
+    {
         int n = nums.size();
         if (n == 1)
             return nums[0];
 
         int start = 0, end = n - 1;
-        while (start <= end) {
+        while (start <= end)
+        {
             int mid = start + ((end - start) / 2);
 
             if (mid == 0 && nums[0] != nums[1])
@@ -52,16 +55,24 @@ public:
             if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1])
                 return nums[mid];
 
-            if (mid % 2 == 0) {
-                if (nums[mid - 1] == nums[mid]) {
+            if (mid % 2 == 0)
+            {
+                if (nums[mid - 1] == nums[mid])
+                {
                     end = mid - 1;
-                } else {
+                }
+                else
+                {
                     start = mid + 1;
                 }
-            } else {
-                if (nums[mid - 1] == nums[mid]) {
+            }
+            else
+            {
+                if (nums[mid - 1] == nums[mid])
+                {
                     start = mid + 1;
-                } else
+                }
+                else
                     end = mid - 1;
             }
         }
